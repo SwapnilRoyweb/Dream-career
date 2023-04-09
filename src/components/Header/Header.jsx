4562e2d4147319537a327b394410613be9016ab0
swapnilRoyWeb <swapnilroyweb@gmail.com>
2023-04-09 22:27:27 +0600

    header done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import '../../index.css';
 
 const Header = () => {
     return (
-        <div>
-            <Link to='/' className=''>Home</Link>
-            <Link to="/statistics">Statistics</Link>
-            <Link to='/appliedJobs'>Applied Jobs</Link>
-            <Link to='/blog'>Blog</Link>
+        <div className='flex justify-evenly mt-5'>
+            <h2 className='text-3xl font-extrabold'>Dream<span className='text-purple-500'>Career</span></h2>
+            <div className='flex gap-5 text-gray-500 font-medium'>
+                <Link to='/'>Home</Link>
+                <Link to="/statistics">Statistics</Link>
+                <Link to='/appliedJobs'>Applied Jobs</Link>
+                <Link to='/blog'>Blog</Link>
+            </div>
+            <button className='bg-purple-500 p-3 rounded-lg text-white font-bold'>Star Applying</button>
         </div>
     );
 };
